e9b96a6909eea7218dfb4a7d0a985875bc71be55
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 22:25:19 +0700

    Initial Commit

---
+++ JOB11/src/job11/convertVocal2angka.java
@@ -0,0 +1,10 @@
+package job11;
+import java.util.Scanner;
+public class convertVocal2angka {
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+}

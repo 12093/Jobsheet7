bf3385129e0203a820a0cb297d313371fae84407
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 22:26:10 +0700

    Tambah Input

--- JOB11/src/job11/convertVocal2angka.java
+++ JOB11/src/job11/convertVocal2angka.java
@@ -6,5 +6,14 @@ public class convertVocal2angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
+    public static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
     
 }

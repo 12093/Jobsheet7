52071a6f7e2bf711e29223dd8f0780b7a241456c
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 21:58:31 +0700

    Coba method length & isEmpty

--- JOB11/src/job11/cobaOperasiiString.java
+++ JOB11/src/job11/cobaOperasiiString.java
@@ -5,4 +5,15 @@ public class cobaOperasiiString {
         String identitas = "Taufiqy Firdaus Jatu/ XRPL2/ 36";
         System.out.println("identitas : "+ identitas);
     }
+    String x = "Operasi";
+    {
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : "+ y);
+        System.out.println("y adalah kosong : "+ y.isEmpty());
+    }
 }

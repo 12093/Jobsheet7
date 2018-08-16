cec3f00894537dff51b4fd4ec2a0b80b27d3d092
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 21:53:25 +0700

    Initial Commit

---
+++ JOB11/src/job11/cobaOperasiiString.java
@@ -0,0 +1,8 @@
+package job11;
+import java.util.Locale;
+public class cobaOperasiiString {
+    public static void main(String[] args) {
+        String identitas = "Taufiqy Firdaus Jatu/ XRPL2/ 36";
+        System.out.println("identitas : "+ identitas);
+    }
+}

b7f1a212dc0b7f87aa5668e7411e20e3f2731fa2
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-09-13 14:55:42 +0700

    Hitung Fibonacci

--- JOB7/src/JOB7/Tugas1.java
+++ JOB7/src/JOB7/Tugas1.java
@@ -5,6 +5,12 @@ public class Tugas1 {
     public static void main(String[] args) {
         String identitas = "Taufiqy Firdaus Jatu / XRPL2 / 36";
         System.out.println(identitas);
-       
+        int [] nominal =  {3000, 2000, 500, 200, 100};
+        Scanner sistem = new Scanner(System.in);
+        System.out.print("Masukkan nominal : ");
+        int uang = sistem.nextInt();
+        
+        
+        
     }
 }

a644e71d83cb6d11beb3a3308fc460b480303034
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-09-13 13:14:05 +0700

    Tambah Tampil Hasil

--- JOB7/src/JOB7/HitungFibonacci.java
+++ JOB7/src/JOB7/HitungFibonacci.java
@@ -29,5 +29,8 @@ public class HitungFibonacci {
         
         return hasil[n-1];
     }
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
+    }
     
 }

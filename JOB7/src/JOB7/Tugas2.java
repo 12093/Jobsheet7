dc60cd98b06a7d0e658f2788b5c22cc460659214
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-09-13 14:57:10 +0700

    Initial Commit

---
+++ JOB7/src/JOB7/Tugas2.java
@@ -0,0 +1,9 @@
+package JOB7;
+import java.util.Scanner;
+public class Tugas2 {
+    public static void main(String[] args) {
+        String identitas = "Taufiqy Firdaus Jatu / XRPL2 / 36";
+        System.out.println(identitas);
+        
+    }
+}

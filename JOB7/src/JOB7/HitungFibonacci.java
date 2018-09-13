796f35a95ae5fb91c8f7040bb5f3e55eed0433ea
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-09-13 13:03:04 +0700

    Initial Commit

---
+++ JOB7/src/JOB7/HitungFibonacci.java
@@ -0,0 +1,10 @@
+package JOB7;
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+}

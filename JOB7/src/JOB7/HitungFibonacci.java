f0f7cc8cf827ef06eec3dbd2e982aa13dfa852c3
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-09-13 13:06:32 +0700

    Tambah Input

--- JOB7/src/JOB7/HitungFibonacci.java
+++ JOB7/src/JOB7/HitungFibonacci.java
@@ -1,10 +1,19 @@
 package JOB7;
+import java.util.Scanner;
 public class HitungFibonacci {
     private static void tampilJudul(String identitas) {
         System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    private static int tampilInput() {
+        Scanner sistem = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = sistem.nextInt();
+        
+        return n;
     }
     
 }
